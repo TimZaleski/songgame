83c8147e4c1437f87ef485429d03c259b5a9c667
Tim Zaleski <zaleskitimothy@yahoo.com>
2021-04-13 13:50:36 -0600

    auth added

--- songgame.client/src/AuthConfig.js
+++ songgame.client/src/AuthConfig.js
@@ -1,3 +1,3 @@
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-6i60ds7c.us.auth0.com'
+export const clientId = 'rynGENu1RmMHRkz8kkVLAmY39M33Niw7'
+export const audience = 'https://songgame-timz'
